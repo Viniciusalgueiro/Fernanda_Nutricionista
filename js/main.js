0e4766a8525c7f9819fc75dc634643724098b379
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2020-10-10 16:26:11 -0300

    Implementando algoritimo de IMC

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
-<script>
-    console.log("olá Mundo");
-</script>
+var titulo = document.querySelector(".titulo");
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdpeso = paciente.querySelector(".info-peso");
+
+    var peso = tdpeso.textContent;
+    var tdaltura = paciente.querySelector(".info-altura");
+    var altura = tdaltura.textContent;
+
+    var tdIMC = paciente.querySelector(".info-imc");
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if (peso <= 0 || peso >= 300) {
+        console.log("peso inválido");
+        pesoValido = false;
+        tdIMC.textContent = "peso inválido!";
+    }
+    if (altura < 0.5 || altura > 2.50) {
+        console.log("altura inválida");
+        tdIMC.textContent = "altura inválida!";
+        alturaValida = false;
+    }
+    if (alturaValida && pesoValido) {
+        var imc = peso / (altura ** 2);
+        tdIMC.textContent = imc.toFixed(2);
+
+    }
+}
+
