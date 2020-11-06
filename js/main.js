5c7e367d09518f28122d0407a1794dee04a981bc
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2020-11-06 16:45:08 -0300

    adicionando novos clientes na tabela

--- js/main.js
+++ js/main.js
@@ -20,11 +20,15 @@ for (var i = 0; i < pacientes.length; i++) {
         console.log("peso inválido");
         pesoValido = false;
         tdIMC.textContent = "peso inválido!";
+        paciente.classList.add("paciente-invalido");
     }
     if (altura < 0.5 || altura > 2.50) {
         console.log("altura inválida");
-        tdIMC.textContent = "altura inválida!";
         alturaValida = false;
+        tdIMC.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+
+
     }
     if (alturaValida && pesoValido) {
         var imc = peso / (altura ** 2);
@@ -33,3 +37,35 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
+var botaodd = document.querySelector("#adicionar-paciente");
+botaodd.addEventListener("click",function (event) {
+    event.preventDefault();
+    var form = document.querySelector("#formadd");
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+
+    nomeTd.textContent = form.nome.value;
+    pesoTd.textContent = form.peso.value;
+    alturaTd.textContent = form.altura.value;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+
+
+
+    console.log("clicado");
+});
