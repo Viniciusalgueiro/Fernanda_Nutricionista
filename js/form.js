e3a590c82c41c51eb6e538c3e767e194be28043a
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2020-11-19 00:08:50 -0300

    adding a removing funciton in the table

--- js/form.js
+++ js/form.js
@@ -50,7 +50,7 @@ function obtemPacienteDoFormulario(form) {
 
 function montaTr(paciente) {
     var pacienteTr = document.createElement("tr");
-    pacienteTr.classList.add("paciente");
+    pacienteTr.classList.add("paciente","fadeOut");
 
     pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
     pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
